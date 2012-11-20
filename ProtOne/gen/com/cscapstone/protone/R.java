e03871bcf06c8621cfef767054a545ec0939e772
Austin Williams <williaah@plu.edu>
2012-11-19 22:36:07 -0800

    Commit updates to bin classes

--- ProtOne/gen/com/cscapstone/protone/R.java
+++ ProtOne/gen/com/cscapstone/protone/R.java
@@ -20,14 +20,16 @@ public final class R {
         public static final int latitudeTextView=0x7f070004;
         public static final int longitudeLabel=0x7f070003;
         public static final int longitudeTextView=0x7f070002;
-        public static final int menu_settings=0x7f070007;
+        public static final int menu_settings=0x7f070008;
         public static final int pointOfInterestName=0x7f070000;
+        public static final int rowTextView=0x7f070007;
         public static final int welcomeButton=0x7f070006;
     }
     public static final class layout {
         public static final int activity_info=0x7f030000;
         public static final int activity_location=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int row=0x7f030003;
     }
     public static final class menu {
         public static final int activity_info=0x7f060000;
@@ -39,6 +41,7 @@ public final class R {
         public static final int hello_world=0x7f040001;
         public static final int latitude=0x7f040004;
         public static final int longitude=0x7f040005;
+        public static final int main_no_items=0x7f04000b;
         public static final int menu_settings=0x7f040002;
         public static final int poi_url=0x7f04000a;
         public static final int title_activity_info=0x7f040009;
